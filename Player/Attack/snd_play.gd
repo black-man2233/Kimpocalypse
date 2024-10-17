c5091493b63490a7a55758868dd749f8b763bc99
SaltedFishy03 <Seekerbrutus@gmail.com>
2024-10-17 14:07:48 +0200

    hotfix

--- Player/Attack/snd_play.gd
+++ Player/Attack/snd_play.gd
@@ -6,3 +6,8 @@ var speed = 100
 var damage = 5
 var knockback = 100
 var attack_size = 1.0
+
+var target = Vector2.ZERO
+var angle = Vector2.ZERO
+
+@onready var player = get_tree().get_first_node_in_group("player")
